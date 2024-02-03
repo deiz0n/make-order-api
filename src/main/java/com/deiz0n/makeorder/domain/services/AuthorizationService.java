cbe9cb28d5df238478ab6919c9fa642c9c1a2c3e
deiz0n <eduardomacedosilva@outlook.com>
2024-02-03 18:29:47 -0300

    Criação do AuthorizationService e criação do método de verificação de email

---
+++ src/main/java/com/deiz0n/makeorder/domain/services/AuthorizationService.java
@@ -0,0 +1,18 @@
+package com.deiz0n.makeorder.domain.services;
+
+import com.deiz0n.makeorder.domain.repositories.FuncionarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    private FuncionarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findFirstByEmail(username);
+    }
+}
