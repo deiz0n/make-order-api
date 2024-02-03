ac648712def122724bb01f7481ef09246a83939a
deiz0n <eduardomacedosilva@outlook.com>
2024-02-03 19:14:52 -0300

    Criação do AuthenticationDTO e criação do método encode no SecurityConfig

--- src/main/java/com/deiz0n/makeorder/api/controllers/AutheticationController.java
+++ src/main/java/com/deiz0n/makeorder/api/controllers/AutheticationController.java
@@ -1,9 +1,33 @@
 package com.deiz0n.makeorder.api.controllers;
 
+import com.deiz0n.makeorder.domain.dtos.AuthenticationDTO;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1.0/auth")
 public class AutheticationController {
+
+    private AuthenticationManager authenticationManager;
+
+    public AutheticationController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Transactional
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody AuthenticationDTO authenticationDTO) {
+        var login = new UsernamePasswordAuthenticationToken(authenticationDTO.getEmail(), authenticationDTO.getSenha());
+        var auth = this.authenticationManager.authenticate(login);
+
+        return ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/com/deiz0n/makeorder/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/deiz0n/makeorder/infrastructure/security/SecurityConfig.java
@@ -3,9 +3,13 @@ package com.deiz0n.makeorder.infrastructure.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,7 +30,17 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .build();
-
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
