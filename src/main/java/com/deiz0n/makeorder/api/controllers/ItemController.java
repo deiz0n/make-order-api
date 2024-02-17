0e54640610759a51547bf167a29228c7734b014e
deiz0n <eduardomacedosilva@outlook.com>
2024-02-17 14:40:17 -0300

    Criação dos endpoints updateItem e deleteItem no ItemController

--- src/main/java/com/deiz0n/makeorder/api/controllers/ItemController.java
+++ src/main/java/com/deiz0n/makeorder/api/controllers/ItemController.java
@@ -1,38 +1,57 @@
 package com.deiz0n.makeorder.api.controllers;
 
 import com.deiz0n.makeorder.domain.dtos.ItemDTO;
+import com.deiz0n.makeorder.domain.models.Item;
 import com.deiz0n.makeorder.domain.services.ItemService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1.0/itens")
 public class ItemController {
 
-    private ItemService itemService;
+    private ItemService service;
 
     public ItemController(ItemService itemService) {
-        this.itemService = itemService;
+        this.service = itemService;
     }
 
     @GetMapping
     public ResponseEntity<List<ItemDTO>> getItens() {
-        List<ItemDTO> itens = itemService.getResouces();
+        List<ItemDTO> itens = service.getResouces();
         return ResponseEntity.ok().body(itens);
     }
 
+    @Transactional
     @PostMapping("/create")
-    public ResponseEntity<ItemDTO> createItem(@RequestBody ItemDTO itemDTO) {
-        var item = itemService.createResource(itemDTO);
+    public ResponseEntity<Item> createItem(@RequestBody ItemDTO newItemRequest) {
+        var item = service.createResource(newItemRequest);
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequestUri()
                 .path("{id}")
                 .buildAndExpand(item.getId())
                 .toUri();
         return ResponseEntity.created(uri).body(item);
+    }
+
+    @Transactional
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Item> updateItem(@RequestBody @Valid ItemDTO newItemRequest, @PathVariable UUID id) {
+        var item = service.updateResource(newItemRequest, id);
+        return ResponseEntity.ok().body(item);
+    }
+
+    @Transactional
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteItem(@PathVariable UUID id) {
+        service.deleteResource(id);
+        return ResponseEntity.noContent().build();
     }
 }
