aa34d24b7bdf18806581065049c092fe3b2e313d
deiz0n <eduardomacedosilva@outlook.com>
2024-02-18 22:08:13 -0300

    Criação do projeto

---
+++ src/main/java/com/deiz0n/makeorderapi/MakeOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.deiz0n.makeorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeOrderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeOrderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deiz0n/makeorderapi/MakeOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deiz0n.makeorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeOrderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
