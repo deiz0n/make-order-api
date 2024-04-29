d7bc3317058456e333aee712bd6bd695009af9fb
deiz0n <eduardomacedosilva@outlook.com>
2024-04-29 17:58:44 -0300

    refactor: Agora um ItensPedido é criado ao criar um Pedido

--- src/main/java/com/deiz0n/makeorderapi/adapters/services/PedidoService.java
+++ src/main/java/com/deiz0n/makeorderapi/adapters/services/PedidoService.java
@@ -4,9 +4,11 @@ import com.deiz0n.makeorderapi.core.domain.dtos.PedidoDTO;
 import com.deiz0n.makeorderapi.core.domain.enums.StatusPedido;
 import com.deiz0n.makeorderapi.core.exceptions.PedidoNotFoundException;
 import com.deiz0n.makeorderapi.infrastructure.config.ModelMapperConfig;
+import com.deiz0n.makeorderapi.infrastructure.entities.ItensPedido;
 import com.deiz0n.makeorderapi.infrastructure.entities.Pedido;
+import com.deiz0n.makeorderapi.infrastructure.persistence.impl.ItensPedidoRepositoryImpl;
 import com.deiz0n.makeorderapi.infrastructure.persistence.impl.PedidoRepositoryImpl;
-import com.deiz0n.makeorderapi.useCases.pedido.*;
+import com.deiz0n.makeorderapi.usecases.pedido.*;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -18,19 +20,31 @@ public class PedidoService implements CreatePedidoUseCase, DeletePedidoUseCase, 
         GetPedidoUseCase, UpdatePedidoUseCase, UpdateStatusPedidoUseCase {
 
     private PedidoRepositoryImpl pedidoRepository;
+    private ItensPedidoRepositoryImpl itensPedidoRepository;
     private ModelMapperConfig mapperConfig;
 
-    public PedidoService(PedidoRepositoryImpl pedidoRepository, ModelMapperConfig mapperConfig) {
+    public PedidoService(PedidoRepositoryImpl pedidoRepository, ModelMapperConfig mapperConfig, ItensPedidoRepositoryImpl itensPedidoRepository) {
         this.pedidoRepository = pedidoRepository;
         this.mapperConfig = mapperConfig;
+        this.itensPedidoRepository = itensPedidoRepository;
     }
 
     @Override
     public PedidoDTO createPedido(Pedido newPedidoRequest) {
         newPedidoRequest.setData(Instant.now());
-        newPedidoRequest.setStatusPedido(StatusPedido.PENDENTE);
+        newPedidoRequest.setStatus(StatusPedido.PENDENTE);
 
         var pedido = pedidoRepository.createPedido(newPedidoRequest);
+
+        for (int i = 0; i < pedido.getItens().size(); i++) {
+            itensPedidoRepository.createItensPedido(
+                    new ItensPedido(
+                            UUID.randomUUID(),
+                            pedido.getItens().get(i).getQuantidade(),
+                            pedido.getItens().get(i).getItem(),
+                            pedido));
+        }
+
         return mapperConfig.modelMapper().map(pedido, PedidoDTO.class);
     }
 
