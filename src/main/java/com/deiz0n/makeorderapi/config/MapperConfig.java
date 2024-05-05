f4a98eb79d0ab2cb9be4b79c483ff0af1d3a3225
deiz0n <eduardomacedosilva@outlook.com>
2024-05-05 19:43:13 -0300

    refactor: Renomeação do MapperConfig

--- src/main/java/com/deiz0n/makeorderapi/config/MapperConfig.java
+++ src/main/java/com/deiz0n/makeorderapi/config/MapperConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ModelMapperConfig {
+public class MapperConfig {
 
     @Bean
-    public ModelMapper modelMapper() {
+    public ModelMapper mapper() {
         return new ModelMapper();
     }
 
