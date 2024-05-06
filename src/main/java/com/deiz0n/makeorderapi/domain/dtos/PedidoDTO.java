f9131ee7ab094cc998cc6c31d078b9521bb08252
deiz0n <eduardomacedosilva@outlook.com>
2024-05-06 10:40:25 -0300

    fix: Correção na visualização do PedidoDTO

--- src/main/java/com/deiz0n/makeorderapi/domain/dtos/PedidoDTO.java
+++ src/main/java/com/deiz0n/makeorderapi/domain/dtos/PedidoDTO.java
@@ -5,6 +5,8 @@ import com.deiz0n.makeorderapi.domain.enums.StatusPedido;
 import com.deiz0n.makeorderapi.domain.entities.Comanda;
 import com.deiz0n.makeorderapi.domain.entities.ItensPedido;
 import com.deiz0n.makeorderapi.domain.entities.Mesa;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,10 +21,14 @@ import java.util.UUID;
 @NoArgsConstructor
 public class PedidoDTO {
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private UUID id;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Instant data;
+    @JsonProperty(value = "forma_pagamento")
     private FormaPagamento formaPagamento;
     private StatusPedido status;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Integer codigo;
     private String observacoes;
     private List<ItensPedido> itens;
@@ -30,7 +36,7 @@ public class PedidoDTO {
     private FuncionarioDTO funcionario;
     private Mesa mesa;
 
-    @JsonProperty(value = "valor_total")
+    @JsonProperty(value = "valor_total", access = JsonProperty.Access.READ_ONLY)
     public Double getValorTotal() {
         var soma = 0.0;
         for (ItensPedido iten : itens) {
