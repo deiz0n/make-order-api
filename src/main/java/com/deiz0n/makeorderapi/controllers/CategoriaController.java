1a708aba7a1276887ae27936974ee3f0ce3f7f8c
deiz0n <eduardomacedosilva@outlook.com>
2024-05-05 21:30:39 -0300

    feat: Criação do CategoriaController

---
+++ src/main/java/com/deiz0n/makeorderapi/controllers/CategoriaController.java
@@ -0,0 +1,51 @@
+package com.deiz0n.makeorderapi.controllers;
+
+import com.deiz0n.makeorderapi.domain.dtos.CategoriaDTO;
+import com.deiz0n.makeorderapi.domain.entities.Categoria;
+import com.deiz0n.makeorderapi.services.CategoriaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v2.0/categorias")
+public class CategoriaController {
+
+    private final CategoriaService service;
+
+    public CategoriaController(CategoriaService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> getCategorias() {
+        var categorias = service.getAll();
+        return ResponseEntity.ok(categorias);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoriaDTO> getCategoria(@PathVariable UUID id) {
+        var categoria = service.getById(id);
+        return ResponseEntity.ok(categoria);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<CategoriaDTO> createCategoria(@RequestBody Categoria request) {
+        var categoria = service.create(request);
+        var uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("{id}")
+                .buildAndExpand()
+                .toUri();
+        return ResponseEntity.created(uri).body(categoria);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteCategoria(@PathVariable UUID id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
