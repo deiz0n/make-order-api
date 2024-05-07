6f68ea87008a096c3b49e62d4d866f755aa1de18
deiz0n <eduardomacedosilva@outlook.com>
2024-05-07 14:41:59 -0300

    fix: Tratamento das exceções pertencentes ao ResourceNotFoundException

--- src/main/java/com/deiz0n/makeorderapi/controllers/HandleExceptionController.java
+++ src/main/java/com/deiz0n/makeorderapi/controllers/HandleExceptionController.java
@@ -1,7 +1,27 @@
 package com.deiz0n.makeorderapi.controllers;
 
+import com.deiz0n.makeorderapi.domain.exceptions.ResourceNotFoundException;
+import com.deiz0n.makeorderapi.domain.utils.Error;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
 
 @ControllerAdvice
 public class HandleExceptionController {
+
+    @ExceptionHandler({ResourceNotFoundException.class})
+    public ResponseEntity<Error> handleResourceNotFound(ResourceNotFoundException exception, HttpServletRequest request) {
+        var error = new Error(
+                Instant.now(),
+                "Recurso não encontrado",
+                exception.getMessage(),
+                HttpStatus.NOT_FOUND,
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 }
