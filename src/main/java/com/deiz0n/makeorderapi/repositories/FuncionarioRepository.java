95088ea6896868794b6839028e02ff1f5e60e6f9
deiz0n <eduardomacedosilva@outlook.com>
2024-05-12 19:43:51 -0300

    feat: Criação dos métodos findByCpf e findByEmail no FuncionarioRepository

--- src/main/java/com/deiz0n/makeorderapi/repositories/FuncionarioRepository.java
+++ src/main/java/com/deiz0n/makeorderapi/repositories/FuncionarioRepository.java
@@ -3,7 +3,12 @@ package com.deiz0n.makeorderapi.repositories;
 import com.deiz0n.makeorderapi.domain.entities.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, UUID> {
+
+    Optional<Funcionario> findByCpf(String cpf);
+    Optional<Funcionario> findByEmail(String email);
+
 }
