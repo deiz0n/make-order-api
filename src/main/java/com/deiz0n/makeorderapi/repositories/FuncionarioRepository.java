042d78b6aa11d904d383cf75507963084d216df2
deiz0n <eduardomacedosilva@outlook.com>
2024-05-12 19:15:50 -0300

    feat: Criação do FuncionarioRepository

---
+++ src/main/java/com/deiz0n/makeorderapi/repositories/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package com.deiz0n.makeorderapi.repositories;
+
+import com.deiz0n.makeorderapi.domain.entities.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, UUID> {
+}
