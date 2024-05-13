c9d3264c1a5016dd611eb692e24c69975326e92b
deiz0n <eduardomacedosilva@outlook.com>
2024-05-12 21:30:35 -0300

    feat: Criação da exceção FuncionarioExistingException

---
+++ src/main/java/com/deiz0n/makeorderapi/domain/exceptions/FuncionarioExistingException.java
@@ -0,0 +1,8 @@
+package com.deiz0n.makeorderapi.domain.exceptions;
+
+public class FuncionarioExistingException extends ResourceExistingException {
+
+    public FuncionarioExistingException(String msg) {
+        super(msg);
+    }
+}
