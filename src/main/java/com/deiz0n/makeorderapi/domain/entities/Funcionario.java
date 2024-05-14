02ab634e804f1aa2f3f5b77d380f8de4d2417cf3
deiz0n <eduardomacedosilva@outlook.com>
2024-05-14 15:56:10 -0300

    feat: Validação dos atributos CPF e Email do Funcionario

--- src/main/java/com/deiz0n/makeorderapi/domain/entities/Funcionario.java
+++ src/main/java/com/deiz0n/makeorderapi/domain/entities/Funcionario.java
@@ -4,7 +4,9 @@ import com.deiz0n.makeorderapi.domain.enums.Setor;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.util.Date;
 import java.util.List;
@@ -23,9 +25,11 @@ public class Funcionario {
     private UUID id;
     @Column(nullable = false, length = 100)
     private String nome;
+    @CPF(message = "CPF inválido")
     @Column(unique = true, nullable = false, length = 11)
     private String cpf;
     @Column(unique = true, nullable = false, length = 50)
+    @Email(message = "Email inválido")
     private String email;
     @Column(nullable = false, length = 100)
     private String senha;
