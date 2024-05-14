6297e119daf0aab88d9b654e3d92d205674e8175
deiz0n <eduardomacedosilva@outlook.com>
2024-05-14 15:49:37 -0300

    feat: Criação do FuncionarioController

---
+++ src/main/java/com/deiz0n/makeorderapi/controllers/FuncionarioController.java
@@ -0,0 +1,60 @@
+package com.deiz0n.makeorderapi.controllers;
+
+import com.deiz0n.makeorderapi.domain.dtos.FuncionarioDTO;
+import com.deiz0n.makeorderapi.domain.entities.Funcionario;
+import com.deiz0n.makeorderapi.services.FuncionarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v2.0/funcionarios")
+public class FuncionarioController {
+
+    private FuncionarioService service;
+
+    public FuncionarioController(FuncionarioService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FuncionarioDTO>> getFuncionarios() {
+        var funcionarios = service.getAll();
+        return ResponseEntity.ok(funcionarios);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FuncionarioDTO> getFuncionario(@PathVariable UUID id) {
+        var funcionario = service.getById(id);
+        return ResponseEntity.ok(funcionario);
+    }
+
+    @Transactional
+    @PostMapping("/create")
+    public ResponseEntity<FuncionarioDTO> createFuncionario(@RequestBody Funcionario request) {
+        var funcionario = service.create(request);
+        var uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("{id}")
+                .buildAndExpand(funcionario.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(funcionario);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteFuncionario(@PathVariable UUID id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<FuncionarioDTO> updateFuncionario(@PathVariable UUID id, @RequestBody Funcionario request) {
+        var funcionario = service.update(id, request);
+        return ResponseEntity.ok(funcionario);
+    }
+
+}
