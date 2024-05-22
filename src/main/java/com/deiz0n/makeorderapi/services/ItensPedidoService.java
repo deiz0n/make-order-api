f4e244f0cdcd83f1b4325632207db27556919d05
deiz0n <eduardomacedosilva@outlook.com>
2024-05-22 14:35:26 -0300

    refactor: ItensPedidoService agora é utilizado como ouvinte para os ItensPedidoEvent

---
+++ src/main/java/com/deiz0n/makeorderapi/services/ItensPedidoService.java
@@ -0,0 +1,32 @@
+package com.deiz0n.makeorderapi.services;
+
+import com.deiz0n.makeorderapi.event.ItensPedidoEvent;
+import com.deiz0n.makeorderapi.repositories.ItensPedidoRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItensPedidoService {
+
+    private ItensPedidoRepository itensPedidoRepository;
+
+    public ItensPedidoService(ItensPedidoRepository itensPedidoRepository) {
+        this.itensPedidoRepository = itensPedidoRepository;
+    }
+
+    @EventListener
+    public void createListener(ItensPedidoEvent event) {
+        itensPedidoRepository.save(event.getItensPedido());
+    }
+
+    @EventListener
+    public void updateListener(ItensPedidoEvent event) {
+        var itensPedido = itensPedidoRepository.getReferenceById(event.getId());
+
+        BeanUtils.copyProperties(event.getItensPedido(), itensPedido, "id", "pedido_id");
+
+        itensPedidoRepository.save(itensPedido);
+    }
+
+}
