f3e469108a31f7002412c8a69e53e70c273a7154
deiz0n <eduardomacedosilva@outlook.com>
2024-05-22 14:36:33 -0300

    refactor: Agora é utilizado para representar eventos ItensPedido

---
+++ src/main/java/com/deiz0n/makeorderapi/event/ItensPedidoEvent.java
@@ -0,0 +1,26 @@
+package com.deiz0n.makeorderapi.event;
+
+import com.deiz0n.makeorderapi.domain.entities.ItensPedido;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+import java.util.UUID;
+
+@Getter
+public class ItensPedidoEvent extends ApplicationEvent {
+
+    private ItensPedido itensPedido;
+    private UUID id;
+
+    public ItensPedidoEvent(Object object, ItensPedido itensPedido) {
+        super(object);
+        this.itensPedido = itensPedido;
+    }
+
+    public ItensPedidoEvent(Object source, ItensPedido itensPedido, UUID id) {
+        super(source);
+        this.itensPedido = itensPedido;
+        this.id = id;
+    }
+
+}
