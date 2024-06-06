7657c2cf82205dea174691c677bc8a9685b24661
deiz0n <eduardomacedosilva@outlook.com>
2024-06-05 21:48:26 -0300

    fix: Captura de exceção ao inserir um CPF ou Email inválidos

--- src/main/java/com/deiz0n/makeorderapi/controllers/HandleExceptionController.java
+++ src/main/java/com/deiz0n/makeorderapi/controllers/HandleExceptionController.java
@@ -4,15 +4,20 @@ import com.deiz0n.makeorderapi.domain.exceptions.ResourceExistingException;
 import com.deiz0n.makeorderapi.domain.exceptions.ResourceNotFoundException;
 import com.deiz0n.makeorderapi.domain.utils.Error;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.Instant;
 
 @ControllerAdvice
-public class HandleExceptionController {
+public class HandleExceptionController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ResourceNotFoundException.class})
     public ResponseEntity<Error> handleResourceNotFound(ResourceNotFoundException exception, HttpServletRequest request) {
@@ -36,5 +41,17 @@ public class HandleExceptionController {
                 request.getRequestURI()
         );
         return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        var error = new Error(
+                Instant.now(),
+                "Campo inválido",
+                ex.getFieldError().getDefaultMessage(),
+                HttpStatus.BAD_REQUEST,
+                request.getDescription(false)
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
