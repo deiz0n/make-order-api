043f1fa6872f9f4d382cb67bb56fccc700b76c49
deiz0n <eduardomacedosilva@outlook.com>
2024-06-06 14:29:32 -0300

    feat: Criação do MesaService

---
+++ src/main/java/com/deiz0n/makeorderapi/services/MesaService.java
@@ -0,0 +1,45 @@
+package com.deiz0n.makeorderapi.services;
+
+import com.deiz0n.makeorderapi.domain.dtos.MesaDTO;
+import com.deiz0n.makeorderapi.domain.entities.Mesa;
+import com.deiz0n.makeorderapi.domain.exceptions.MesaNotFoundException;
+import com.deiz0n.makeorderapi.repositories.MesaRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class MesaService {
+
+    private MesaRepository repository;
+
+    public MesaService(MesaRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<MesaDTO> getAll() {
+        return repository.findAll()
+                .stream()
+                .map(mesa -> new MesaDTO(mesa.getId(), mesa.getNumero(), mesa.getCliente()))
+                .collect(Collectors.toList());
+    }
+
+    public MesaDTO getById(UUID id) {
+        return repository.findById(id)
+                .map(mesa -> new MesaDTO(mesa.getId(), mesa.getNumero(), mesa.getCliente()))
+                .orElseThrow(() -> new MesaNotFoundException("Não foi possível encontrar uma mesa com o id informado"));
+    }
+
+    public MesaDTO create(Mesa newMesa) {
+        var mesa = repository.save(newMesa);
+        return new MesaDTO(mesa.getId(), mesa.getNumero(), mesa.getCliente());
+    }
+
+    public void delete(UUID id) {
+        var mesa = getById(id);
+        repository.deleteById(mesa.id());
+    }
+}
