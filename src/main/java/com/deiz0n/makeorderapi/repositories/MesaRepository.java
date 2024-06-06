d907f2d43c5322ac6bebebc4765ee9c71dbdf7b9
deiz0n <eduardomacedosilva@outlook.com>
2024-06-06 14:10:02 -0300

    feat: Criação do MesaRepository

---
+++ src/main/java/com/deiz0n/makeorderapi/repositories/MesaRepository.java
@@ -0,0 +1,9 @@
+package com.deiz0n.makeorderapi.repositories;
+
+import com.deiz0n.makeorderapi.domain.entities.Mesa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MesaRepository extends JpaRepository<Mesa, UUID> {
+}
