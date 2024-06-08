0fd3c65700c449f8fc2de1b050eb845df660762f
deiz0n <eduardomacedosilva@outlook.com>
2024-06-08 20:04:58 -0300

    refactor: Criação do pacote Infrastructure

--- src/main/java/com/deiz0n/makeorderapi/infrastructure/config/MapperConfig.java
+++ src/main/java/com/deiz0n/makeorderapi/infrastructure/config/MapperConfig.java
@@ -1,4 +1,4 @@
-package com.deiz0n.makeorderapi.config;
+package com.deiz0n.makeorderapi.infrastructure.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
