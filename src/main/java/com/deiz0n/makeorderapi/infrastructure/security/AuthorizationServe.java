db3d6aaf5c152420f9211ebaefa1808fa5214e71
deiz0n <eduardomacedosilva@outlook.com>
2024-06-08 20:20:46 -0300

    feat: Criação do AuthorizationServe

---
+++ src/main/java/com/deiz0n/makeorderapi/infrastructure/security/AuthorizationServe.java
@@ -0,0 +1,23 @@
+package com.deiz0n.makeorderapi.infrastructure.security;
+
+import com.deiz0n.makeorderapi.repositories.FuncionarioRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+public class AuthorizationServe implements UserDetailsService {
+
+    private FuncionarioRepository repository;
+
+    public AuthorizationServe(FuncionarioRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.getByUsername(username);
+    }
+}
