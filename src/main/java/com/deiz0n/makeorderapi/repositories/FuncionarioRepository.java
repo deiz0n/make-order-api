a8819303becc2a7e616b5f1d4c19c1ac76b9921e
deiz0n <eduardomacedosilva@outlook.com>
2024-06-08 20:20:33 -0300

    feat: Criação da query getByUsername

--- src/main/java/com/deiz0n/makeorderapi/repositories/FuncionarioRepository.java
+++ src/main/java/com/deiz0n/makeorderapi/repositories/FuncionarioRepository.java
@@ -2,6 +2,7 @@ package com.deiz0n.makeorderapi.repositories;
 
 import com.deiz0n.makeorderapi.domain.entities.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -10,5 +11,6 @@ public interface FuncionarioRepository extends JpaRepository<Funcionario, UUID> 
 
     Optional<Funcionario> findByCpf(String cpf);
     Optional<Funcionario> findByEmail(String email);
+    UserDetails getByUsername(String email);
 
 }
