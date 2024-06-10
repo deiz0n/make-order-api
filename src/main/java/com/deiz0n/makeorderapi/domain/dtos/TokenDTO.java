89f846a89f79155048f34f3ade27efbb248ed40f
deiz0n <eduardomacedosilva@outlook.com>
2024-06-10 11:30:28 -0300

    feat: Criação do TokenDTO

---
+++ src/main/java/com/deiz0n/makeorderapi/domain/dtos/TokenDTO.java
@@ -0,0 +1,14 @@
+package com.deiz0n.makeorderapi.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDTO {
+
+    private String token;
+
+}
