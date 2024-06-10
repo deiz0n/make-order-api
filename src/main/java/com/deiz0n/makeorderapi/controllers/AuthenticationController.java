ab4c909f304355839329618000030c4bc20bae03
deiz0n <eduardomacedosilva@outlook.com>
2024-06-10 11:30:41 -0300

    feat: Criação do AuthenticationController

---
+++ src/main/java/com/deiz0n/makeorderapi/controllers/AuthenticationController.java
@@ -0,0 +1,36 @@
+package com.deiz0n.makeorderapi.controllers;
+
+import com.deiz0n.makeorderapi.domain.dtos.AuthenticationDTO;
+import com.deiz0n.makeorderapi.domain.dtos.TokenDTO;
+import com.deiz0n.makeorderapi.domain.entities.Funcionario;
+import com.deiz0n.makeorderapi.infrastructure.security.TokenService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2.0/authentication")
+public class AuthenticationController {
+
+    private AuthenticationManager authenticationManager;
+    private TokenService service;
+
+    public AuthenticationController(AuthenticationManager authenticationManager, TokenService service) {
+        this.authenticationManager = authenticationManager;
+        this.service = service;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDTO> singIn(@RequestBody AuthenticationDTO request) {
+        var user = new UsernamePasswordAuthenticationToken(request.getEmail(), request.getSenha());
+        var authentication = authenticationManager.authenticate(user);
+
+        var token = service.generateToken((Funcionario) authentication.getPrincipal());
+        return ResponseEntity.ok(new TokenDTO(token));
+    }
+
+}
