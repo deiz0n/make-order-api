fefe88a1d8a319d6e2dacd2b8dc5793c1b8ef063
deiz0n <eduardomacedosilva@outlook.com>
2024-06-18 10:48:44 -0300

    feat: Criação do RecoveryPasswordResponse

---
+++ src/main/java/com/deiz0n/makeorderapi/domain/utils/RecoveryPasswordResponse.java
@@ -0,0 +1,14 @@
+package com.deiz0n.makeorderapi.domain.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecoveryPasswordResponse {
+
+    private String email;
+
+}
