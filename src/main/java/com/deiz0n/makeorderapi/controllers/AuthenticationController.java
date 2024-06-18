05ee4c711809cb7f21c0d90707a64ba27651c545
deiz0n <eduardomacedosilva@outlook.com>
2024-06-18 10:51:10 -0300

    feat: Criação do endpoint recovery para recuperação de senha

--- src/main/java/com/deiz0n/makeorderapi/controllers/AuthenticationController.java
+++ src/main/java/com/deiz0n/makeorderapi/controllers/AuthenticationController.java
@@ -3,7 +3,9 @@ package com.deiz0n.makeorderapi.controllers;
 import com.deiz0n.makeorderapi.domain.dtos.AuthenticationDTO;
 import com.deiz0n.makeorderapi.domain.dtos.TokenDTO;
 import com.deiz0n.makeorderapi.domain.entities.Funcionario;
+import com.deiz0n.makeorderapi.domain.utils.RecoveryPasswordResponse;
 import com.deiz0n.makeorderapi.infrastructure.security.TokenService;
+import com.deiz0n.makeorderapi.services.FuncionarioService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,15 +16,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v2.0/authentication")
+@RequestMapping("/api/v2.0/auth")
 public class AuthenticationController {
 
     private AuthenticationManager authenticationManager;
-    private TokenService service;
+    private TokenService tokenService;
+    private FuncionarioService funcionarioService;
 
-    public AuthenticationController(AuthenticationManager authenticationManager, TokenService service) {
+    public AuthenticationController(AuthenticationManager authenticationManager, TokenService tokenService, FuncionarioService funcionarioService) {
         this.authenticationManager = authenticationManager;
-        this.service = service;
+        this.tokenService = tokenService;
+        this.funcionarioService = funcionarioService;
     }
 
     @PostMapping("/login")
@@ -30,8 +34,14 @@ public class AuthenticationController {
         var user = new UsernamePasswordAuthenticationToken(request.getEmail(), request.getSenha());
         var authentication = authenticationManager.authenticate(user);
 
-        var token = service.generateToken((Funcionario) authentication.getPrincipal());
+        var token = tokenService.generateToken((Funcionario) authentication.getPrincipal());
         return ResponseEntity.ok(new TokenDTO(token));
+    }
+
+    @PostMapping("/recovery")
+    public ResponseEntity<?> recoveryPassword(@RequestBody RecoveryPasswordResponse request) {
+        funcionarioService.recovery(request.getEmail());
+        return ResponseEntity.ok("Verify you email");
     }
 
 }
