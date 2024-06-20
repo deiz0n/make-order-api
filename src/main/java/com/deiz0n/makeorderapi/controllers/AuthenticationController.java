6123fcc6a9c65ecd96e4398a5138fe70dada3cd0
deiz0n <eduardomacedosilva@outlook.com>
2024-06-19 21:47:39 -0300

    refactor: Toda lógica do AuthenticationController foi movida para o AuthenticationSrevice

--- src/main/java/com/deiz0n/makeorderapi/controllers/AuthenticationController.java
+++ src/main/java/com/deiz0n/makeorderapi/controllers/AuthenticationController.java
@@ -1,10 +1,11 @@
 package com.deiz0n.makeorderapi.controllers;
 
-import com.deiz0n.makeorderapi.domain.dtos.AuthenticationDTO;
-import com.deiz0n.makeorderapi.domain.dtos.TokenDTO;
+import com.deiz0n.makeorderapi.domain.utils.requests.AuthenticationRequest;
+import com.deiz0n.makeorderapi.domain.utils.responses.TokenResponse;
 import com.deiz0n.makeorderapi.domain.entities.Funcionario;
-import com.deiz0n.makeorderapi.domain.utils.RecoveryPasswordResponse;
+import com.deiz0n.makeorderapi.domain.utils.requests.RecoveryPasswordRequest;
 import com.deiz0n.makeorderapi.infrastructure.security.TokenService;
+import com.deiz0n.makeorderapi.services.AuthenticationService;
 import com.deiz0n.makeorderapi.services.FuncionarioService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -19,27 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v2.0/auth")
 public class AuthenticationController {
 
-    private AuthenticationManager authenticationManager;
-    private TokenService tokenService;
+    private AuthenticationService authenticationService;
     private FuncionarioService funcionarioService;
 
-    public AuthenticationController(AuthenticationManager authenticationManager, TokenService tokenService, FuncionarioService funcionarioService) {
-        this.authenticationManager = authenticationManager;
-        this.tokenService = tokenService;
+    public AuthenticationController(AuthenticationService authenticationService, FuncionarioService funcionarioService) {
+        this.authenticationService = authenticationService;
         this.funcionarioService = funcionarioService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TokenDTO> singIn(@RequestBody @Valid AuthenticationDTO request) {
-        var user = new UsernamePasswordAuthenticationToken(request.getEmail(), request.getSenha());
-        var authentication = authenticationManager.authenticate(user);
-
-        var token = tokenService.generateToken((Funcionario) authentication.getPrincipal());
-        return ResponseEntity.ok(new TokenDTO(token));
+    public ResponseEntity<TokenResponse> singInAuth(@RequestBody @Valid AuthenticationRequest request) {
+        var token = authenticationService.singIn(request);
+        return ResponseEntity.ok(token);
     }
 
     @PostMapping("/recovery")
-    public ResponseEntity<?> recoveryPassword(@RequestBody RecoveryPasswordResponse request) {
+    public ResponseEntity<?> recoveryPassword(@RequestBody RecoveryPasswordRequest request) {
         funcionarioService.recovery(request.getEmail());
         return ResponseEntity.ok("Verify you email");
     }
