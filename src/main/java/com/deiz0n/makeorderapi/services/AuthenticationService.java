a16dc651082a59fd6cff78d5a8253df9b345ca03
deiz0n <eduardomacedosilva@outlook.com>
2024-06-24 21:33:48 -0300

    feat: Criação do método resetPassword no AuthenticationService

--- src/main/java/com/deiz0n/makeorderapi/services/AuthenticationService.java
+++ src/main/java/com/deiz0n/makeorderapi/services/AuthenticationService.java
@@ -3,13 +3,17 @@ package com.deiz0n.makeorderapi.services;
 import com.deiz0n.makeorderapi.domain.entities.Funcionario;
 import com.deiz0n.makeorderapi.domain.events.GenerateTokenEvent;
 import com.deiz0n.makeorderapi.domain.events.GeneratedTokenEvent;
+import com.deiz0n.makeorderapi.domain.events.ResetedPasswordEvent;
 import com.deiz0n.makeorderapi.domain.utils.requests.AuthenticationRequest;
+import com.deiz0n.makeorderapi.domain.utils.requests.ResetPasswordRequest;
 import com.deiz0n.makeorderapi.domain.utils.responses.TokenResponse;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class AuthenticationService {
@@ -36,5 +40,10 @@ public class AuthenticationService {
     @EventListener
     public void getToken(GeneratedTokenEvent event) {
         token = event.getResponse();
+    }
+
+    public void resetPassword(ResetPasswordRequest newData, UUID funcionarioId) {
+        var resetedPasswordEvent = new ResetedPasswordEvent(funcionarioId, newData);
+        eventPublisher.publishEvent(resetedPasswordEvent);
     }
 }
