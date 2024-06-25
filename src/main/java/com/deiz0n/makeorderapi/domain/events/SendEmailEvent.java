48800958943676397152e294e92a196d23d4f164
deiz0n <eduardomacedosilva@outlook.com>
2024-06-24 21:33:08 -0300

    refactor: O SendEmailEvent agora retorna o usuário que realizou a requisição

--- src/main/java/com/deiz0n/makeorderapi/domain/events/SendEmailEvent.java
+++ src/main/java/com/deiz0n/makeorderapi/domain/events/SendEmailEvent.java
@@ -1,15 +1,16 @@
 package com.deiz0n.makeorderapi.domain.events;
 
+import com.deiz0n.makeorderapi.domain.dtos.FuncionarioDTO;
 import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
 @Getter
 public class SendEmailEvent extends ApplicationEvent {
 
-    private String email;
+    private FuncionarioDTO funcionario;
 
-    public SendEmailEvent(Object source, String email) {
+    public SendEmailEvent(Object source, FuncionarioDTO funcionario) {
         super(source);
-        this.email = email;
+        this.funcionario = funcionario;
     }
 }
