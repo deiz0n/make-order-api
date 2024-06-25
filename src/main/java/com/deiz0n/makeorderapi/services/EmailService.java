22b4d1bcd69533b624f9a12658004ca95f81d171
deiz0n <eduardomacedosilva@outlook.com>
2024-06-24 21:36:06 -0300

    refactor: Modificações no corpo da mensagem ao enviar o email

--- src/main/java/com/deiz0n/makeorderapi/services/EmailService.java
+++ src/main/java/com/deiz0n/makeorderapi/services/EmailService.java
@@ -2,8 +2,6 @@ package com.deiz0n.makeorderapi.services;
 
 import com.deiz0n.makeorderapi.domain.events.SendEmailEvent;
 import com.deiz0n.makeorderapi.domain.exceptions.SendEmailException;
-import com.deiz0n.makeorderapi.domain.utils.ConfirmCode;
-import com.deiz0n.makeorderapi.domain.utils.GenerateCode;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
@@ -11,6 +9,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.Instant;
 import java.util.Date;
 
@@ -18,13 +18,14 @@ import java.util.Date;
 @Getter
 public class EmailService {
 
-    private JavaMailSender mailSender;
-
     @Value("${spring.mail.username}")
     private String emailFrom;
-    private ConfirmCode code = new ConfirmCode(GenerateCode.generate());
 
-    public EmailService(JavaMailSender mailSender) {
+    private JavaMailSender mailSender;
+
+    private final URI uri = new URI("localhost:8080/api/v2.0/auth/recovery/user?id=");
+
+    public EmailService(JavaMailSender mailSender) throws URISyntaxException {
         this.mailSender = mailSender;
     }
 
@@ -32,13 +33,15 @@ public class EmailService {
     private void send(SendEmailEvent event) {
         var message = new SimpleMailMessage();
 
+        var url = uri+event.getFuncionario().getId().toString();
+
         message.setSentDate(Date.from(Instant.now()));
         try {
 
             message.setFrom(emailFrom);
-            message.setTo(event.getEmail());
+            message.setTo(event.getFuncionario().getEmail());
             message.setSubject("Recuperação de senha");
-            message.setText("Eis o seu código de recuperação: " + code);
+            message.setText("Acesse o seguinte endereço para alterar sua senha: " + url);
 
             mailSender.send(message);
         } catch (Exception e) {
