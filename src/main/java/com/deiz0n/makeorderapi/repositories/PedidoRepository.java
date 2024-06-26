e26a27e6f590ec2b05a94c1d76a39e5b760541ba
deiz0n <eduardomacedosilva@outlook.com>
2024-06-26 16:10:24 -0300

    feat: Criação da query getTopFuncionarios no PedidoRepository

--- src/main/java/com/deiz0n/makeorderapi/repositories/PedidoRepository.java
+++ src/main/java/com/deiz0n/makeorderapi/repositories/PedidoRepository.java
@@ -9,7 +9,9 @@ import java.util.UUID;
 
 public interface PedidoRepository extends JpaRepository<Pedido, UUID> {
 
-    @Query("SELECT I.item AS ITEM, COUNT(P.id) AS QNTD_VENDAS " +
+    @Query("SELECT " +
+                "I.item AS ITEM, " +
+                "COUNT(P.id) AS QNTD_VENDAS " +
             "FROM tb_pedido P " +
             "INNER JOIN tb_itens_pedido I " +
             "ON P.id = I.pedido.id " +
@@ -18,4 +20,18 @@ public interface PedidoRepository extends JpaRepository<Pedido, UUID> {
             "LIMIT 3")
     List<Object> getTopItens();
 
+    @Query("SELECT " +
+                "(F.id," +
+                "F.nome, " +
+                "F.email, " +
+                "F.dataNascimento, " +
+                "F.setor) AS FUNCIONARIO, " +
+                "COUNT(P.funcionario.id) AS VENDAS " +
+            "FROM tb_pedido P " +
+            "INNER JOIN tb_funcionario F " +
+            "ON P.funcionario.id = F.id " +
+            "GROUP BY F.id " +
+            "ORDER BY VENDAS DESC " +
+            "LIMIT 3")
+    List<Object> getTopFuncionaios();
 }
