dc5ba04bb802dfe62934c8335241ee721ce6bfe9
deiz0n <eduardomacedosilva@outlook.com>
2024-06-30 19:14:19 -0300

    test: Ajustes nos testes do EmailService

--- src/test/java/com/deiz0n/makeorderapi/services/EmailServiceTest.java
+++ src/test/java/com/deiz0n/makeorderapi/services/EmailServiceTest.java
@@ -3,17 +3,19 @@ package com.deiz0n.makeorderapi.services;
 import com.deiz0n.makeorderapi.domain.dtos.FuncionarioDTO;
 import com.deiz0n.makeorderapi.domain.events.SendEmailEvent;
 
+import com.deiz0n.makeorderapi.domain.exceptions.SendEmailException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -27,8 +29,7 @@ class EmailServiceTest {
     private JavaMailSender mailSender;
 
     private SendEmailEvent emailEvent;
-    @Value("${spring.mail.username}")
-    private String emailFrom;
+    private final String emailFrom = "email@from.com";
 
     @BeforeEach
     void setUp() {
@@ -39,9 +40,23 @@ class EmailServiceTest {
     @Test
     void whenSendThenDontReturn() {
         doNothing().when(eventPublisher).publishEvent(emailEvent);
-        doNothing().when(mailSender).send(new SimpleMailMessage());
+        doNothing().when(mailSender).send(any(SimpleMailMessage.class));
 
         service.send(emailEvent);
+
+        verify(mailSender, times(1)).send(any(SimpleMailMessage.class));
+    }
+
+    @Test
+    void whenSendThenThrowSendEmailException()  {
+        doThrow(SendEmailException.class).when(mailSender).send(any(SimpleMailMessage.class));
+
+        var exception = assertThrows(
+                SendEmailException.class,
+                () -> service.send(emailEvent)
+        );
+
+        assertEquals("Erro ao enviar email", exception.getMessage());
     }
 
     private void mockData() {
