c8ae70078c1ee8002f647e9784b387f29945af6b
deiz0n <eduardomacedosilva@outlook.com>
2024-07-01 14:36:36 -0300

    fix: Ajustes nos testes do CategoriaController

--- src/test/java/com/deiz0n/makeorderapi/controllers/CategoriaControllerTest.java
+++ src/test/java/com/deiz0n/makeorderapi/controllers/CategoriaControllerTest.java
@@ -2,38 +2,52 @@ package com.deiz0n.makeorderapi.controllers;
 
 import com.deiz0n.makeorderapi.domain.dtos.CategoriaDTO;
 import com.deiz0n.makeorderapi.domain.entities.Categoria;
+import com.deiz0n.makeorderapi.domain.utils.responses.ResponseRequest;
 import com.deiz0n.makeorderapi.services.CategoriaService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.net.URI;
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class CategoriaControllerTest {
 
-    public static final Integer INDEX = 0;
     public static final UUID ID = UUID.randomUUID();
     public static final String NOME = "Categoria 1";
+    public static final String DUDU = "dudu";
 
-    @InjectMocks
+    @MockBean
     private CategoriaController controller;
-    @Mock
+    @MockBean
     private CategoriaService service;
+    @Autowired
+    private MockMvc mockMvc;
 
-    private Categoria categoria;
     private CategoriaDTO categoriaDTO;
+    private ResponseRequest responseRequest;
+
 
     @BeforeEach
     void setUp() {
@@ -42,70 +56,84 @@ class CategoriaControllerTest {
     }
 
     @Test
-    void whenGetCategoriasThenReturnHttpOk() {
-        when(service.getAll()).thenReturn(List.of(categoriaDTO));
+    void whenGetCategoriasThenReturnHttpOk() throws Exception {
+        when(controller.getCategorias()).thenReturn(ResponseEntity.ok(List.of(categoriaDTO)));
 
-        ResponseEntity<List<CategoriaDTO>> responseList = controller.getCategorias();
-
-        assertNotNull(responseList);
-        assertNotNull(responseList.getBody());
-
-        assertEquals(ResponseEntity.class, responseList.getClass());
-        assertEquals(HttpStatus.OK, responseList.getStatusCode());
-
-        assertEquals(ID, responseList.getBody().get(INDEX).getId());
-        assertEquals(NOME, responseList.getBody().get(INDEX).getNome());
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v2.0/categorias")
+                .accept(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+                        .with(user(DUDU)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0]").isNotEmpty())
+                .andExpect(jsonPath("$[0].id").value(ID.toString()))
+                .andExpect(jsonPath("$[0].nome").value(NOME));
     }
 
     @Test
-    void whenGetCategoriaThenReturnHttpOk() {
-        when(service.getById(any(UUID.class))).thenReturn(categoriaDTO);
+    void whenGetCategoriaThenReturnHttpOk() throws Exception {
+        when(controller.getCategoria(any(UUID.class))).thenReturn(ResponseEntity.ok(categoriaDTO));
 
-        ResponseEntity<CategoriaDTO> response = controller.getCategoria(ID);
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v2.0/categorias/{id}", ID)
+                .accept(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+                        .with(user(DUDU)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$.id").value(ID.toString()))
+                .andExpect(jsonPath("$.nome").value(NOME));
 
-        assertNotNull(response);
-        assertNotNull(response.getBody());
-
-        assertEquals(ResponseEntity.class, response.getClass());
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-
-        assertEquals(ID, response.getBody().getId());
-        assertEquals(NOME, response.getBody().getNome());
     }
 
     @Test
-    void whenCreateCategoriaThenReturnHttpCreated() {
-        when(service.create(any())).thenReturn(categoriaDTO);
+    void whenCreateCategoriaThenReturnHttpCreated() throws Exception {
+        when(controller.createCategoria(any())).thenReturn(ResponseEntity.created(any(URI.class)).body(categoriaDTO));
 
-        ResponseEntity<CategoriaDTO> response = controller.createCategoria(categoria);
-
-        assertNotNull(response);
-        assertNotNull(response.getBody());
-        assertNotNull(response.getHeaders().get("Location"));
-
-        assertEquals(ResponseEntity.class, response.getClass());
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-
-        assertEquals(ID, response.getBody().getId());
-        assertEquals(NOME, response.getBody().getNome());
+        mockMvc.perform( MockMvcRequestBuilders
+                .post("/api/v2.0/categorias/create")
+                .accept(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+                        .with(user(DUDU))
+                        .content(toJson(new Categoria(null, "Categoria")))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$.id").value(ID.toString()))
+                .andExpect(jsonPath("$.nome").value(NOME));
     }
 
     @Test
-    void whenDeleteCategoriaThenReturnHttpNoContent() {
-        doNothing().when(service).delete(any(UUID.class));
+    void whenDeleteCategoriaThenReturnHttpNoContent() throws Exception {
+        when(controller.deleteCategoria(any(UUID.class))).thenReturn(ResponseEntity.status(HttpStatus.NO_CONTENT).body(responseRequest));
 
-        ResponseEntity<?> response = controller.deleteCategoria(ID);
-
-        assertNotNull(response);
-
-        assertEquals(ResponseEntity.class, response.getClass());
-        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
-
-        verify(service, times(1)).delete(any(UUID.class));
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete("/api/v2.0/categorias/delete/{id}", ID)
+                .accept(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+                        .with(user(DUDU)))
+                .andExpect(status().isNoContent())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$.instant").isNotEmpty())
+                .andExpect(jsonPath("$.title").value(responseRequest.getTitle()))
+                .andExpect(jsonPath("$.description").value(responseRequest.getDescription()))
+                .andExpect(jsonPath("$.status").value(responseRequest.getStatus().toString()));
     }
 
-    public void mockData() {
-        categoria = new Categoria(ID, NOME);
+    private void mockData() {
         categoriaDTO = new CategoriaDTO(ID, NOME);
+        responseRequest = new ResponseRequest(
+                Instant.now(),
+                "Recurso excluído",
+                String.format("A categoria com id: {%s} foi excluída com sucesso", ID.toString()),
+                HttpStatus.NO_CONTENT.value());
+    }
+
+    private String toJson(Object categoria) {
+        try {
+            return new ObjectMapper().writeValueAsString(categoria);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException();
+        }
     }
 }
