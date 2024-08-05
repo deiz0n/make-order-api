acce8ea1153225c0ff1ca23389d6f37863d2aae7
deiz0n <eduardomacedosilva@outlook.com>
2024-08-05 16:30:36 -0300

    Deploy na heroku

--- src/main/resources/db/migration/V1.0__db-inicial.sql
+++ src/main/resources/db/migration/V1.0__db-inicial.sql
@@ -1,62 +1,62 @@
-create table tb_categoria (
-    id uuid not null,
-    nome varchar(50) not null unique,
-    primary key (id)
+CREATE TABLE IF NOT EXISTS tb_categoria(
+                                           id uuid not null primary key,
+                                           nome varchar(50)
 );
 
-create table tb_comanda (
-    id uuid not null,
-    primary key (id)
+CREATE TABLE IF NOT EXISTS tb_comanda(
+                                         id uuid not null primary key,
+                                         id_pedido uuid
 );
 
-create table tb_funcionario (
-    setor smallint not null check (setor between 0 and 2),
-    data_nascimento timestamp(6) not null,
-    cpf varchar(11) not null unique,
-    id uuid not null,
-    email varchar(50) not null unique,
-    nome varchar(100) not null,
-    senha varchar(100) not null,
-    primary key (id)
+CREATE TABLE IF NOT EXISTS tb_funcionario(
+                                             id uuid not null primary key,
+                                             nome varchar(100) not null,
+                                             cpf varchar(11) not null unique,
+                                             email varchar(100) not null unique,
+                                             senha varchar(255) not null,
+                                             data_nascimento timestamp not null,
+                                             setor smallint not null check (setor between 0 and 2),
+                                             sub_id uuid not null,
+                                             id_pedido uuid,
+                                             foreign key (id_pedido) references tb_pedido(id)
 );
 
-create table tb_item (
-    preco numeric(38,2) not null,
-    quantidade_disponivel integer not null,
-    categoria_id uuid, id uuid not null,
-    nome varchar(50) not null unique,
-    descricao text not null,
-    primary key (id)
+CREATE TABLE IF NOT EXISTS tb_item(
+                                      id uuid not null primary key,
+                                      nome varchar(50) not null,
+                                      preco numeric(38,2) not null,
+                                      descricao text not null,
+                                      quantidade_disponivel integer not null,
+                                      id_categoria uuid,
+                                      foreign key(id_categoria) references tb_categoria(id)
 );
 
-create table tb_itens_pedido (
-    quantidade integer,
-    id uuid not null,
-    item_id uuid, pedido_id uuid,
-    primary key (id)
+CREATE TABLE IF NOT EXISTS tb_itens_pedido(
+                                              id uuid not null primary key,
+                                              quantidade integer not null,
+                                              id_item uuid,
+                                              id_pedido uuid,
+                                              foreign key(id_item) references tb_item(id),
+                                              foreign key(id_pedido) references tb_pedido(id)
 );
 
-create table tb_mesa (
-    numero integer not null unique,
-    id uuid not null,
-    cliente varchar(75) not null unique,
-    primary key (id)
+CREATE TABLE IF NOT EXISTS tb_mesa(
+                                      id uuid not null primary key,
+                                      numero integer not null,
+                                      cliente varchar(50)
 );
 
-create table tb_pedido (
-    codigo integer,
-    forma_pagamento smallint not null check (forma_pagamento between 0 and 3),
-    status smallint not null check (status between 0 and 2),
-    data timestamp(6) with time zone not null,
-    comanda_id uuid,
-    funcionario_id uuid,
-    id uuid not null, mesa_id uuid,
-    observacoes text not null,
-    primary key (id));
-
-alter table if exists tb_item add constraint FK_categoria_item foreign key (categoria_id) references tb_categoria;
-alter table if exists tb_itens_pedido add constraint FK_item_itens_pedido foreign key (item_id) references tb_item;
-alter table if exists tb_itens_pedido add constraint FK_pedido_itens_pedido foreign key (pedido_id) references tb_pedido;
-alter table if exists tb_pedido add constraint FK_comanda_pedido foreign key (comanda_id) references tb_comanda;
-alter table if exists tb_pedido add constraint FK_funcionario_pedido foreign key (funcionario_id) references tb_funcionario;
-alter table if exists tb_pedido add constraint FK_mesa_pedido foreign key (mesa_id) references tb_mesa;
+CREATE TABLE IF NOT EXISTS tb_pedido(
+                                        id uuid not null primary key,
+                                        data timestamp not null,
+                                        forma_pagamento smallint check (forma_pagamento between 0 and 3) not null,
+                                        status smallint check (status between 0 and 2) not null,
+                                        codigo serial not null,
+                                        observacoes text not null,
+                                        id_comanda uuid,
+                                        id_funcionario uuid,
+                                        id_mesa uuid,
+                                        foreign key(id_comanda) references tb_comanda(id),
+                                        foreign key(id_funcionario) references tb_funcionario(id),
+                                        foreign key(id_mesa) references tb_mesa(id)
+);
